954d11b34042562e2d9403592e0f793481997241
wierzbickitomasz <72969906+wierzbickitomasz@users.noreply.github.com>
2021-01-16 21:33:56 +0100

    aa
    aa

--- assets/tests/iso/kafelek.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tileset" tilewidth="64" tileheight="40" tilecount="4" columns="4">
- <image source="tileset.png" width="256" height="40"/>
-</tileset>
